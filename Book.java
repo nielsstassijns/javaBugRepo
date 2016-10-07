b984ab8b048aa12669f09351aa17604cb2466038
nielsstassijns <niels.stassijns@realdolmen.com>
2016-10-07 15:13:27 +0200

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,26 @@
+package com.realdolmen.bookstore;
+
+public class Book {
+
+	private String name;
+
+	public Book(String name) {
+		super();
+		this.name = name
+	}
+
+	public Book() {
+		super();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
